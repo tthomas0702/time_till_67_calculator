fdc9ca6fad317ed5d9a22cbbe106da63e3fdc420
Tim Thomas <cxq5@yahoo.com>
2023-06-08 17:51:30 -0700

    Update time_to_67.sh
    adding main section

--- time_to_67.sh
+++ time_to_67.sh
@@ -1,62 +1,34 @@
 #!/bin/bash
 
 calculate_remaining_age() {
-    local birthday="$1"
+    # Read user's birthday
+    read -p "Enter your birthday (YYYY-MM-DD): " birthday
 
-    # Calculate the current date and time
-    local current_date=$(date +%F)
+    # Get current date
+    current_date=$(date +%F)
 
-    # Calculate the difference between the current date and the birthday
-    local years=$(date -d "$current_date" +%Y)     # Current year
-    local months=$(date -d "$current_date" +%m)    # Current month
-    local days=$(date -d "$current_date" +%d)      # Current day
+    # Calculate remaining time until reaching age 67
+    remaining_time=$(date -u -d "$birthday +67 years" +%s)
+    current_time=$(date -u -d "$current_date" +%s)
+    seconds_remaining=$((remaining_time - current_time))
 
-    # Extract the year, month, and day from the birthday
-    local birth_year=$(date -d "$birthday" +%Y)
-    local birth_month=$(date -d "$birthday" +%m)
-    local birth_day=$(date -d "$birthday" +%d)
-
-    # Calculate the remaining years, months, and days until age 67
-    local target_year=$((birth_year + 67))
-    local target_date="$target_year-$birth_month-$birth_day"
-
-    if [[ "$current_date" > "$target_date" ]]; then
+    # Check if already reached age 67
+    if (( seconds_remaining <= 0 )); then
         echo "You have already reached the age of 67."
     else
-        local remaining_years=$((target_year - years))
-        local remaining_months=$((birth_month - months))
-        local remaining_days=$((birth_day - days))
-
-        if [[ $remaining_months -lt 0 ]]; then
-            remaining_years=$((remaining_years - 1))
-            remaining_months=$((remaining_months + 12))
-        fi
-
-        if [[ $remaining_days -lt 0 ]]; then
-            remaining_months=$((remaining_months - 1))
-
-            case $birth_month in
-                01|03|05|07|08|10|12)
-                    remaining_days=$((remaining_days + 31))
-                    ;;
-                04|06|09|11)
-                    remaining_days=$((remaining_days + 30))
-                    ;;
-                02)
-                    if (( birth_year % 4 == 0 && (birth_year % 100 != 0 || birth_year % 400 == 0) )); then
-                        remaining_days=$((remaining_days + 29))
-                    else
-                        remaining_days=$((remaining_days + 28))
-                    fi
-                    ;;
-            esac
-        fi
-
-        echo "You have $remaining_years years, $remaining_months months, and $remaining_days days remaining until you reach the age of 67."
+        # Calculate remaining years, months, and days
+        years_remaining=$((seconds_remaining / 31536000))
+        months_remaining=$(( (seconds_remaining % 31536000) / 2628000 ))
+        days_remaining=$(( ( (seconds_remaining % 31536000) % 2628000 ) / 86400 ))
+        
+        echo "You have $years_remaining years, $months_remaining months, and $days_remaining days remaining until you reach the age of 67."
     fi
 }
 
-# Read the user's birthday in the format: YYYY-MM-DD
-read -p "Enter your birthday (YYYY-MM-DD): " birthday
+# Main section
+main() {
+    calculate_remaining_age
+}
 
-calculate_remaining_age "$birthday"
+# Execute the main section
+main
