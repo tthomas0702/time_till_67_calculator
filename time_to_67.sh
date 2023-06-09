84d7ec2d836e700a29f5cf3185b6cc92cc5053cf
Tim Thomas <cxq5@yahoo.com>
2023-06-09 06:29:10 -0700

    Update time_to_67.sh

--- time_to_67.sh
+++ time_to_67.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 calculate_remaining_age() {
-    # Read user's birthday
-    read -p "Enter your birthday (YYYY-MM-DD): " birthday
+  # Read user's birthday
+  read -p "Enter your birthday (YYYY-MM-DD): " birthday
 
-    # Get current date
-    current_date=$(date +%F)
+  # Get current date
+  current_date=$(date +%F)
 
-    # Calculate remaining time until reaching age 67
-    remaining_time=$(date -u -d "$birthday +67 years" +%s)
-    current_time=$(date -u -d "$current_date" +%s)
-    seconds_remaining=$((remaining_time - current_time))
+  # Calculate remaining time until reaching age 67
+  remaining_time=$(date -u -d "$birthday +67 years" +%s)
+  current_time=$(date -u -d "$current_date" +%s)
+  seconds_remaining=$((remaining_time - current_time))
 
-    # Check if already reached age 67
-    if (( seconds_remaining <= 0 )); then
-        echo "You have already reached the age of 67."
-    else
-        # Calculate remaining years, months, and days
-        years_remaining=$((seconds_remaining / 31536000))
-        months_remaining=$(( (seconds_remaining % 31536000) / 2628000 ))
-        days_remaining=$(( ( (seconds_remaining % 31536000) % 2628000 ) / 86400 ))
-        
-        echo "You have $years_remaining years, $months_remaining months, and $days_remaining days remaining until you reach the age of 67."
-    fi
+  # Check if already reached age 67
+  if (( seconds_remaining <= 0 )); then
+    echo "You have already reached the age of 67."
+  else
+    # Calculate remaining years, months, and days
+    years_remaining=$((seconds_remaining / 31536000))
+    months_remaining=$(((seconds_remaining % 31536000) / 2628000))
+    days_remaining=$((((seconds_remaining % 31536000) % 2628000) / 86400))
+
+    echo "You have $years_remaining years, $months_remaining months, and $days_remaining days remaining until you reach the age of 67."
+  fi
 }
 
 # Main section
 main() {
-    calculate_remaining_age
+  calculate_remaining_age
 }
 
 # Execute the main section
